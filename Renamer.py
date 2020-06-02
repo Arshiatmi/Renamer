59896ec6af97f7d434d515a69963410419de7771
Arshia <35062789+Arshiaprg@users.noreply.github.com>
2020-06-03 01:40:08 +0430

    Add --mode Option

--- Renamer.py
+++ Renamer.py
@@ -4,9 +4,13 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--name',help='The String You Wanted Rename To.')
 parser.add_argument('--format',help='The Format You Wanted To Change')
-parser.add_argument('--path',required=True,help='The Path Of Folder')
+parser.add_argument('--path',help='The Path Of Folder')
 parser.add_argument('--delimiter',help='Delimiter Of Name Of File And Suffix.')
+parser.add_argument('--mode',help='The Mode. Default Is 1 ( Create Time , Modify Time , Last Access Time )')
 dt = parser.parse_args()
+
+if not dt.path:
+    dt.path = input("Enter Path : ")
 
 if len(sys.argv) > 1:
     os.chdir(dt.path)
@@ -19,7 +23,33 @@ def isFile(file_name):
         return True
     return False
 
-paths = sorted(os.listdir(), key=os.path.getctime)
+paths = ""
+
+if dt.mode:
+    if dt.mode == "1":
+        paths = sorted(os.listdir(), key=os.path.getctime)
+    elif dt.mode == "2":
+        paths = sorted(os.listdir(), key=os.path.getmtime)
+    elif dt.mode == "3":
+        paths = sorted(os.listdir(), key=os.path.getatime)
+    else:
+        paths = sorted(os.listdir(), key=os.path.getctime)
+        x = input("Sorry An Error In Mode ... Default Setted. ( Creation Time )\n Do You Want To Continue ?")
+        if x.lower() != "y" or x.lower() != "yes":
+            exit(0)
+else:
+    while True:
+        try:
+            x = int(input("Enter Mode : ( 1 -> Creation Time / 2 -> Modify Time / 3 -> Last Access Time )"))
+            break
+        except:
+            print("Try Again :(")
+    if x == 1:
+        paths = sorted(os.listdir(), key=os.path.getctime)
+    elif x == 2:
+        paths = sorted(os.listdir(), key=os.path.getmtime)
+    elif x == 3:
+        paths = sorted(os.listdir(), key=os.path.getatime)
 
 exts = set()
 all_files = list(paths)
